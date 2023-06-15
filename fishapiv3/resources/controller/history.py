f5ef2f67335c9972d65f31f0066bce7a85687fd9
MauL08 <akbarma2002@gmail.com>
2023-06-15 20:49:31 +0700

    finish crud seed with history

--- fishapiv3/resources/controller/history.py
+++ fishapiv3/resources/controller/history.py
@@ -2,17 +2,18 @@ from flask import Flask, Response, request, jsonify, current_app, url_for, send_
 from fishapiv3.database.models import *
 from flask_restful import Resource
 from fishapiv3.resources.helper import *
-from datetime import datetime
+import datetime
 import json
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
 from bson.objectid import ObjectId
+from ...database.models import *
 
 class SeedHistoryApi(Resource):
     def get(self):
         try:
-            start_date = datetime.strptime(request.args.get('start_date'), '%Y-%m-%d') if request.args.get('start_date') else ""
-            end_date = datetime.strptime(request.args.get('end_date'), '%Y-%m-%d') if request.args.get('end_date') else ""
+            start_date = datetime.datetime.strptime(request.args.get('start_date'), '%Y-%m-%d') if request.args.get('start_date') else ""
+            end_date = datetime.datetime.strptime(request.args.get('end_date'), '%Y-%m-%d') + datetime.timedelta(days=1) if request.args.get('end_date') else ""
 
             pipeline = [
                 {
@@ -23,7 +24,26 @@ class SeedHistoryApi(Resource):
                         }
                     }
                 },
-                {"$sort": {"fish_seed_id": 1}}
+                {"$sort": {"fish_seed_id": 1}},
+                {'$lookup': {
+                    'from': 'seed_inventory',
+                    'let': {"fishseedid": "$fish_seed_id"},
+                    'pipeline': [
+                        {'$match': {'$expr': {'$eq': ['$_id', '$$fishseedid']}}},
+                        {"$project": {
+                            "_id": 1,
+                            "fish_seed_category": 1,
+                            "fish_type": 1,
+                            "brand_name": 1,
+                            "price": 1,
+                            "created_at": 1,
+                        }}
+                    ],
+                    'as': 'seed'
+                }},
+                {"$addFields": {
+                    "seed": {"$first": "$seed"},
+                }},
             ]
 
             testing = SeedHistory.objects.aggregate(pipeline)
@@ -44,6 +64,12 @@ class SeedHistoryApi(Resource):
                 "pond": request.form.get('pond', None),
             }
 
+            # update seed inventory table
+            get_seed_by_id = SeedInventory.objects.get(id=request.form.get('fish_seed_id', None))
+            get_seed_by_id.amount -= int(request.form.get('usage', None))
+            get_seed_by_id.save()
+            
+            # save body to history table
             seed_history = SeedHistory(**body).save()
             id = seed_history.id
             res = {"message": "success add seed history","id": id, "data": body}
